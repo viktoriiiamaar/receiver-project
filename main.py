8f82c337a53ae06d8358322fe750c906bfcd5714
viktoriiiamaar <vmarushc@nd.edu>
2025-04-09 13:40:22 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def on_received_value(name, value):
     if name == "Do this:":
-        # do something with value (e.g., store in a list, print it, etc.)
         basic.show_number(value)
 
 radio.on_received_value(on_received_value)
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
     if (name == "Do this:") {
-        //  do something with value (e.g., store in a list, print it, etc.)
         basic.showNumber(value)
     }
     
