fac3473c8b674bb17c02a6a60470b19def8c4004
viktoriiiamaar <vmarushc@nd.edu>
2025-04-09 13:42:40 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def on_received_value(name, value):
-    if name == "Do this:":
-        basic.show_number(value)
+def on_forever():
+    def on_received_value(name, value):
+        if name == "Do this:":
+            basic.show_number(value)
 
-radio.on_received_value(on_received_value)
-radio.set_group(1)
+    radio.on_received_value(on_received_value)
+    radio.set_group(1)
+
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    if (name == "Do this:") {
-        basic.showNumber(value)
-    }
-    
+basic.forever(function on_forever() {
+    radio.onReceivedValue(function on_received_value(name: string, value: number) {
+        if (name == "Do this:") {
+            basic.showNumber(value)
+        }
+        
+    })
+    radio.setGroup(1)
 })
-radio.setGroup(1)
