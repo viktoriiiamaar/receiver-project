2605ba7619a850a9c8d44204407da2656b7b136e
viktoriiiamaar <vmarushc@nd.edu>
2025-04-09 21:13:14 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ def on_received_value(name, value):
             serial.write_line("Go to the left")
         elif value == 3:
             serial.write_line("Go to the right")
-        else:
-            serial.write_line("Unknown command: " + str(value))
+
 
 radio.on_received_value(on_received_value)
 radio.set_group(1)
--- main.ts
+++ main.ts
@@ -7,8 +7,6 @@ radio.onReceivedValue(function on_received_value(name: string, value: number) {
             serial.writeLine("Go to the left")
         } else if (value == 3) {
             serial.writeLine("Go to the right")
-        } else {
-            serial.writeLine("Unknown command: " + ("" + value))
         }
         
     }
